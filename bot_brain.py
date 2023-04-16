da0dec3d801c92de28ee7f798aa5f652f898bb7d
arpitsengar99 <arpitsengar99@gmail.com>
2023-04-16 23:38:33 +0530

    modified main.py

--- bot_brain.py
+++ bot_brain.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, \
-    ElementClickInterceptedException
+    ElementClickInterceptedException, NoSuchWindowException
 
 
 class Bot_Brain:
@@ -20,63 +20,54 @@ class Bot_Brain:
         self.driver.find_element(By.ID, "password").send_keys(PASS)
         self.driver.find_element(By.ID, "password").send_keys(Keys.ENTER)
 
-    def toast_remover(self):
-        pass
-
-    #
-    # /html/body/div[4]/div[4]/span[2]
-    # /html/body/div[4]/div[2]/span[2]
-
     def engine(self, TO_CLICK):
         self.driver.get(url="https://vityarthi.com/course/learning/Python-Essentials")
         time.sleep(2)
 
         module_codes = [35, 37, 39, 42, 43, 44, 45, 46, 48, 56, 68, 77, 79]
-        switch_on = True
         for i in TO_CLICK:
-            while switch_on:
-                try:
-                    """opening the module"""
-                    self.driver.find_element(By.ID, f'chapter_{module_codes[i]}').click()
-                    time.sleep(1)
-                    # //*[@id="collapseChapter48"]/div/div[1]/div/div[2]/div/label
-                    j = 0
-                    while True:
-                        try:
-                            j += 1
-                            """clicking video box to open it"""
-                            self.driver.find_element(By.XPATH,
-                                                     f'//*[@id="collapseChapter{module_codes[i]}"]/div/div[{j}]').click()
-                            time.sleep(1)
+            try:
+                """opening the module"""
+                self.driver.find_element(By.ID, f'chapter_{module_codes[i]}').click()
+                time.sleep(1)
 
-                            """clicking checkbox"""
-                            self.driver.find_element(By.XPATH,
-                                                     f'//*[@id="collapseChapter{module_codes[i]}"]/div/div[{j}]/div/div['
-                                                     f'2]/div/label').click()
-                            time.sleep(1)
+                j = 0
+                while True:
+                    try:
+                        j += 1
+                        """clicking video box to open it"""
+                        self.driver.find_element(By.XPATH,
+                                                 f'//*[@id="collapseChapter{module_codes[i]}"]/div/div[{j}]').click()
+                        time.sleep(0.5)
 
-                            """scrolling content page to avoid exception"""
-                            pg.press("pagedown")
-                            time.sleep(2)
+                        """clicking checkbox"""
+                        self.driver.find_element(By.XPATH,
+                                                 f'//*[@id="collapseChapter{module_codes[i]}"]/div/div[{j}]/div/div['
+                                                 f'2]/div/label').click()
+                        time.sleep(0.5)
 
-                        except NoSuchElementException:
-                            print(f"Module {i+1} completed.")
-                            switch_on = False
-                            break
+                        """scrolling content page to avoid exception"""
+                        pg.press("pagedown")
 
-                        except ElementNotInteractableException:
-                            print(f"1. Inner ElementNotInteractableException; module: {i + 1}, vid: {j}")
-                            # self.driver.find_element(By.XPATH, '//*[@id="collapseChapter{module_codes[i]}"]/div/div[{'
-                            #                                    'j}]').send_keys(Keys.PAGE_DOWN)
-                            pass
+                    except NoSuchElementException:
+                        print(f"Module {i+1} completed.")
+                        break
 
-                        except ElementClickInterceptedException:
-                            print(f"2. Inner ElementClickInterceptedException; module: {i + 1}, vid: {j}")
+                    except ElementNotInteractableException:
+                        print(f"1. Inner ElementNotInteractableException; module: {i + 1}, vid: {j}")
+                        pass
 
-                except ElementNotInteractableException:
-                    print(f"1. Outer ElementNotInteractableException:  {i + 1}, vid: {j}")
-                    pass
+                    except ElementClickInterceptedException:
+                        print(f"2. Inner ElementClickInterceptedException; module: {i + 1}, vid: {j}")
 
-                except ElementClickInterceptedException:
-                    print(f"2. Outer ElementClickInterceptedException: Module {i + 1}, vid: {j}")
-                    pass
+            except ElementNotInteractableException:
+                print(f"1. Outer ElementNotInteractableException:  {i + 1}, vid: {j}")
+                pass
+
+            except ElementClickInterceptedException:
+                print(f"2. Outer ElementClickInterceptedException: Module {i + 1}, vid: {j}")
+                pass
+
+            except NoSuchWindowException:
+                print("Window closed.")
+                exit()
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import time
+import json
 from bot_brain import Bot_Brain
-from interface import main_app
+from interface import App
 
 switch_on = False
-user_input = main_app()
+user_input = App().main_app()
 switch_on = True
+
+with
+
 if switch_on:
     app = Bot_Brain()
-    app.login("arpitsengar2022@vitbhopal.ac.in", os.getenv("PASSWORD"))
+    app.login(os.getenv("CGMAIL"), os.getenv("PASSWORD"))
     app.engine(user_input)
 
 time.sleep(600)
